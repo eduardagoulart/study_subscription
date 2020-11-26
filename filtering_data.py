61ff38f9bc939cc02c66826627e65917773f7fa7
eduardagoulart <goularteduarda.a@gmail.com>
2020-11-26 08:01:42 -0300

    filtering data script

---
+++ filtering_data.py
@@ -0,0 +1,40 @@
+import pandas as pd
+
+
+SERVICES = ["Netflix", "Hulu", "CBS All Access", "Starz", "Showtime"]
+
+
+def remove_merchant(df):
+    """
+    This method removed all the services that are not in the list below:
+        - Netflix
+        - Hulu
+        - CBS All Access
+        - Starz
+        - Showtime
+    Parameter:
+        - df: Pandas DataFrame
+    Return
+        - DataFrame
+    """
+    df = df.loc[df["merchant_name"].isin(SERVICES)]
+    return df
+
+
+def filter_description(df):
+    """
+    This method will remove all the rows that does not contain service name
+    in the description
+    Parameter:
+        - df: Pandas DataFrame
+    Return
+        - DataFrame
+    """
+    df = df.loc[df["description"].str.contains("|".join(SERVICES))]
+    return df
+
+
+if __name__ == "__main__":
+    df = pd.read_csv("test_Data.csv")
+    df = remove_merchant(df)
+    df = filter_description(df)
