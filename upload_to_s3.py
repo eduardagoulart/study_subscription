d9b1d2a44b8f7ee2547e49fd205d8e2b9a6b120a
eduardagoulart <goularteduarda.a@gmail.com>
2020-11-26 16:36:20 -0300

    create parquet file instead of csv

--- upload_to_s3.py
+++ upload_to_s3.py
@@ -11,21 +11,21 @@ bucket = s3.Bucket("test-bucket-user1")
 def df_to_csv():
     df = main()
     path = "/Users/eduardagoulart/Documents/study/antena/"
-    name = "output_df.csv"
+    name = "output_df.parquet"
     output = path + name
-    df.to_csv(output)
+    df.to_parquet(output)
     return name
 
 
-def upload_file():
-    path = df_to_csv()
+def upload_file_to_s3():
+    file_name = df_to_csv()
     try:
-        bucket.upload_file(path, bucket)
+        bucket.upload_file(file_name, "parquet-files/{}".format(file_name))
     except ClientError as e:
         logging.error(e)
         return False
-    return bucket + path
+    return "parquet-files/" + file_name
 
 
 if __name__ == "__main__":
-    upload_file()
+    upload_file_to_s3()
