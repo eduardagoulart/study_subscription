0dcc3b57355f4035884ea732dcc736877be1d658
eduardagoulart <goularteduarda.a@gmail.com>
2020-11-27 18:17:49 -0300

    upload filtered_out DFs to s3 as well

--- upload_to_s3.py
+++ upload_to_s3.py
@@ -9,18 +9,21 @@ bucket = s3.Bucket("test-bucket-user1")
 
 
 def df_to_csv():
-    df = main()
+    df, not_final_results = main()
     path = "/Users/eduardagoulart/Documents/study/antena/"
-    name = "output_df.parquet"
-    output = path + name
+    name_matching = "output_df.parquet"
+    name_not_final_results = "not_matching_df.parquet"
+    output = path + name_matching
     df.to_parquet(output)
-    return name
+    not_final_results.to_parquet(path + name_not_final_results)
+    return name_matching, name_not_final_results
 
 
 def upload_file_to_s3():
-    file_name = df_to_csv()
+    file_name, name_not_final_results = df_to_csv()
     try:
         bucket.upload_file(file_name, "parquet-files/{}".format(file_name))
+        bucket.upload_file(file_name, "parquet-files/{}".format(name_not_final_results))
     except ClientError as e:
         logging.error(e)
         return False
